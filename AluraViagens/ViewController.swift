8ec83446f53cdee48f44a92740eab38220e39d06
Matheus Henrick Dias <mhenrickd@gmail.com>
2020-03-24 21:09:08 -0300

    first commit with the base layout

--- AluraViagens/ViewController.swift
+++ AluraViagens/ViewController.swift
@@ -8,13 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var tabelaViagens: UITableView!
+    
+    let listaViagens = ["Rio de Janeiro", "Ceará", "São Paulo"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tabelaViagens.dataSource = self
         // Do any additional setup after loading the view.
     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listaViagens.count
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = listaViagens[indexPath.row]
+        return cell
+    }
 
 }
 
