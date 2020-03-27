d1d9224477d256f49ef8ea97d8a380bd66124a2f
Matheus Henrick Dias <mhenrickd@gmail.com>
2020-03-27 17:56:24 -0300

    adding 'pacotes' and 'detalhes da viagem' screen

---
+++ AluraViagens/Pacotes/Controller/PacotesViagensViewController.swift
@@ -0,0 +1,82 @@
+//
+//  PacotesViagensViewController.swift
+//  AluraViagens
+//
+//  Created by Matheus Henrick Dias on 26/03/20.
+//  Copyright © 2020 Matheus Henrick Dias. All rights reserved.
+//
+
+import UIKit
+
+class PacotesViagensViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate, UICollectionViewDelegate {
+
+
+    @IBOutlet weak var colecaoPacotesViagem: UICollectionView!
+    
+    @IBOutlet weak var labelContadorPacotes: UILabel!
+    
+    @IBOutlet weak var pesquisarViagens: UISearchBar!
+    
+    let listaComTodasViagens:Array<Viagem> = ViagemDAO().retornaTodasAsViagens()
+    var listaViagens:Array<Viagem> = []
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        listaViagens = listaComTodasViagens
+        colecaoPacotesViagem.dataSource = self
+        colecaoPacotesViagem.delegate = self
+        pesquisarViagens.delegate = self
+        self.labelContadorPacotes.text = self.atualizaContadorLabel()
+
+        // Do any additional setup after loading the view.
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.listaViagens.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let celulaPacote = collectionView.dequeueReusableCell(withReuseIdentifier: "celulaPacote", for: indexPath) as! PacoteViagemCollectionViewCell
+        
+        let viagemAtual = listaViagens[indexPath.item]
+        
+        celulaPacote.labelTitulo.text = viagemAtual.titulo
+        celulaPacote.labelQuantidadeDias.text = "\(viagemAtual.quantidadeDeDias) dias"
+        celulaPacote.labelPreco.text = "RS\(viagemAtual.preco)"
+        celulaPacote.imagemViagem.image = UIImage(named: viagemAtual.caminhoDaImagem)
+        
+        celulaPacote.layer.borderWidth = 0.5
+        celulaPacote.layer.cornerRadius = 8
+        return celulaPacote
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let larguraCelula = collectionView.bounds.width / 2
+        return CGSize(width: larguraCelula-15, height: 160)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: "detalhes") as! DetalhesViagensViewController // problema Could not cast value of type 'AluraViagens.PacotesViagensViewController'
+        self.present(controller, animated: true, completion: nil)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        listaViagens = listaComTodasViagens
+        if searchText != "" {
+            let listaFiltrada: [Viagem] = listaViagens.filter { (viagem) -> Bool in
+                viagem.titulo.localizedStandardContains(searchText)
+            }
+            //let filtroListaViagem = NSPredicate(format: "titulo contains[c] %@", searchText) //problema?
+            //rlet listaFiltrada:Array<Viagem> = (listaViagens as NSArray).filtered(using: filtroListaViagem) as! Array
+            listaViagens = listaFiltrada
+        }
+        self.labelContadorPacotes.text = self.atualizaContadorLabel()
+        colecaoPacotesViagem.reloadData()
+    }
+    
+    func atualizaContadorLabel() -> String {
+        return listaViagens.count == 1 ? "1 pacote encontrado" : "\(listaViagens.count) pacotes encontrados"
+    }
+
+}
---
+++ AluraViagens/Pacotes/View/PacoteViagemCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PacoteViagemCollectionViewCell.swift
+//  AluraViagens
+//
+//  Created by Matheus Henrick Dias on 26/03/20.
+//  Copyright © 2020 Matheus Henrick Dias. All rights reserved.
+//
+
+import UIKit
+
+
+class PacoteViagemCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imagemViagem: UIImageView!
+    @IBOutlet weak var labelTitulo: UILabel!
+    @IBOutlet weak var labelQuantidadeDias: UILabel!
+    @IBOutlet weak var labelPreco: UILabel!
+}
